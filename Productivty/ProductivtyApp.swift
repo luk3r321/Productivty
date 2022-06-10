1a42615eeb131ee69b198d6978192b96292413f3
luk3r321 <luk3r321@gmail.com>
2022-06-09 23:41:50 -0400

    Initial Commit

---
+++ Productivty/ProductivtyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivtyApp.swift
+//  Productivty
+//
+//  Created by Luke on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ProductivtyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
