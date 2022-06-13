c71940c459cf9a458681d5dbc55ff868b0aa0a0c
luk3r321 <luk3r321@gmail.com>
2022-06-13 18:59:59 -0400

    Added tabs

--- Productivty/ContentView.swift
+++ Productivty/ContentView.swift
@@ -9,13 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView{
+            FirstView().tabItem{
+                Image(systemName: "person")
+                Text("1st")
+            }
+            SecondView().tabItem{
+                Image(systemName: "house")
+                Text("2nd")
+            }
+            ThirdView().tabItem{
+                Image(systemName: "person")
+                Text("doc")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct FirstView: View{
+    var body: some View{
+        ZStack{
+            Color.red.ignoresSafeArea(edges: .top)
+            Text("First View")
+        }
+    }
+}
+
+struct SecondView: View{
+    var body: some View{
+        ZStack{
+            Color.blue.ignoresSafeArea(edges: .top)
+            Text("Second View")
+        }
+    }
+}
+struct ThirdView: View{
+    var body: some View{
+        ZStack{
+            Color.green.ignoresSafeArea(edges: .top)
+            Text("Third View")
+        }
     }
 }
