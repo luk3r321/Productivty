b205893b81a3d4490c804a6d38e8493f29e4749c
luk3r321 <luk3r321@gmail.com>
2022-06-13 20:06:32 -0400

    Gave tabs new images and titles geared towards the app. Added two more pages

--- Productivty/ContentView.swift
+++ Productivty/ContentView.swift
@@ -11,17 +11,38 @@ struct ContentView: View {
     var body: some View {
         TabView{
             FirstView().tabItem{
-                Image(systemName: "person")
-                Text("1st")
+                Spacer()
+                Image(systemName: "pencil")
+                Spacer()
+                Text("To Do List")
             }
             SecondView().tabItem{
-                Image(systemName: "house")
-                Text("2nd")
+                Image(systemName: "calendar")
+                Spacer()
+                Text("Due Dates")
             }
             ThirdView().tabItem{
-                Image(systemName: "person")
-                Text("doc")
+                Image(systemName: "doc.text")
+                Spacer()
+                Text("Resolution")
             }
+            FourthView().tabItem{
+                Image(systemName: "person.2")
+                Spacer()
+                Text("Social")
+            }
+            FifthView().tabItem{
+               
+                Image(systemName: "person")
+                Spacer()
+                Text("Personal")
+                
+            }
+        }
+        .accentColor(Color.black)
+        .onAppear{
+            UITabBar.appearance().backgroundColor = UIColor.gray
+            UITabBar.appearance().unselectedItemTintColor = UIColor.blue
         }
     }
 }
@@ -36,7 +57,8 @@ struct FirstView: View{
     var body: some View{
         ZStack{
             Color.red.ignoresSafeArea(edges: .top)
-            Text("First View")
+            Text("To Do List").padding().font(.title)
+            Spacer()
         }
     }
 }
@@ -45,7 +67,8 @@ struct SecondView: View{
     var body: some View{
         ZStack{
             Color.blue.ignoresSafeArea(edges: .top)
-            Text("Second View")
+            Text("Due Dates").padding().font(.title)
+            Spacer()
         }
     }
 }
@@ -53,7 +76,27 @@ struct ThirdView: View{
     var body: some View{
         ZStack{
             Color.green.ignoresSafeArea(edges: .top)
-            Text("Third View")
+            Text("Resolution").padding().font(.title)
+            Spacer()
+        }
+    }
+}
+
+struct FourthView: View{
+    var body: some View{
+        ZStack{
+            Color.orange.ignoresSafeArea(edges: .top)
+            Text("Social").padding().font(.title)
+            Spacer()
+        }
+    }
+}
+struct FifthView: View{
+    var body: some View{
+        ZStack{
+            Color.purple.ignoresSafeArea(edges: .top)
+            Text("Personal").padding().font(.title)
+            Spacer()
         }
     }
 }
